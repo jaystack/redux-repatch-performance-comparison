0c2a933dbf86383c0fdec46d9a174f7421472be4
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-08-30 15:25:30 +0200

    add comparison test

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { createStore } = require('redux');
+const { Store } = require('repatch');
+
+const MAX = 10e6;
+
+const INCREMENT = 'INCREMENT';
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+const reduxStore = createStore(reducer);
+const repatchStore = new Store(0);
+
+const reduxIncrement = () => ({ type: INCREMENT });
+const repatchIncrement = () => state => state + 1;
+
+console.time('redux');
+for (let i = 0; i < MAX; ++i) {
+  reduxStore.dispatch(reduxIncrement());
+}
+console.timeEnd('redux');
+
+console.time('repatch');
+for (let i = 0; i < MAX; ++i) {
+  repatchStore.dispatch(repatchIncrement());
+}
+console.timeEnd('repatch');
